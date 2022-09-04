359a9f0621d9bc627663f72ff2e9d9da38882d4a
Alfonso Puerta <apuertaj2@gmail.com>
2022-09-04 01:10:57 -0500

    Preparing the project for deployment

--- server/app.js
+++ server/app.js
@@ -2,14 +2,18 @@ import express from 'express'
 import morgan from 'morgan'
 import fileUpload from 'express-fileupload'
 import cors from 'cors'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
 import postsRoutes from './posts/posts.routes.js';
 import handleErrors from './app/handleErrors.middleware.js'
 
+// Initialize
 const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // Middlewares
-app.use(cors({ origin: 'http://localhost:5173' }))
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(fileUpload({
@@ -17,9 +21,12 @@ app.use(fileUpload({
   tempFileDir: './upload',
 }))
 
+// Static files
+app.use(express.static(join(__dirname, '../client/dist')))
+
 // Routes
 app.use('/api/posts', postsRoutes)
 app.use(handleErrors.notFound)
 app.use(handleErrors.handleErrors)
 
-export default app
+export { app }
