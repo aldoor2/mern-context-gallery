4f0cf26202f6ae702fe1a4ff9719d8b3cb1326f9
Alphonse <apuertaj2@gmail.com>
2022-09-04 00:33:13 -0500

    Updated the request for uploading images to the api by editing the post

--- client/src/api/posts.js
+++ client/src/api/posts.js
@@ -1,21 +1,31 @@
 import axios from 'axios'
 
-export const getPostsRequest = async () => await axios.get('http://localhost:4000/api/posts')
+export const getPostsRequest = async () => await axios.get('/api/posts')
 
 export const createPostRequest = async (post) => {
   const form = new FormData()
 
   Object.keys(post).forEach(key => form.append(key, post[key]));
 
-  return await axios.post('http://localhost:4000/api/posts', form, {
+  return await axios.post('/api/posts', form, {
     headers: {
       'Content-Type': 'multipart/form-data'
     }
   })
 }
 
-export const deletePostRequest = async (postId) => await axios.delete(`http://localhost:4000/api/posts/${postId}`)
+export const deletePostRequest = async (postId) => await axios.delete(`/api/posts/${postId}`)
 
-export const getPostRequest = async (postId) => await axios.get(`http://localhost:4000/api/posts/${postId}`)
+export const getPostRequest = async (postId) => await axios.get(`/api/posts/${postId}`)
 
-export const updatePostRequest = async (postId, newFields) => await axios.patch(`http://localhost:4000/api/posts/${postId}`, newFields)
+export const updatePostRequest = async (postId, newPostFields) => {
+  const form = new FormData()
+
+  Object.keys(newPostFields).forEach(key => form.append(key, newPostFields[key]))
+
+  return await axios.patch(`/api/posts/${postId}`, form, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  })
+}
